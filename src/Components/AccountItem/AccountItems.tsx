1f04cb680453adb8ca7b4e7d787868eb402355b3
Bebravee <pavebogatenko@yandex.ru>
2025-06-22 10:47:38 +0300

    all

--- src/Components/AccountItem/AccountItems.tsx
+++ src/Components/AccountItem/AccountItems.tsx
@@ -6,6 +6,14 @@ const AccountItem: React.FC = () => {
       <div className="AccountItems__header">
         <input type="text" placeholder="Поиск" />
       </div>
+
+      <div className="AccountItems__items">123</div>
+
+      <div className="AccountItems__footer">
+        <button className="AccountItems__footer-button">
+          Добавить аккаунт
+        </button>
+      </div>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WindowControls from "./Components/WindowControls/WindowControls";
-
-import AuthenticationPage from "./Pages/AuthenticationPage.tsx";
-//import AccountsListPage from "./Pages/AccountsListPage/AccountsListPage";
+import AccountsListPage from "./Pages/AccountsListPage/AccountsListPage";
 import "./Assets/Styles/MainStyles.scss";
 
 const router = createBrowserRouter([
@@ -14,7 +12,7 @@ const router = createBrowserRouter([
     element: (
       <div>
         <WindowControls />
-        <AuthenticationPage />
+        <AccountsListPage />
       </div>
     ),
   },
